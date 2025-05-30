bc8067eaf7f8337a6377ef339db0b45fa6762ca5
Harsh-wd <harshbansal.it26@gmail.com>
2025-05-30 07:09:46 +0530

    day 4 arg func added

---
+++ day4/func2.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+///argument with return value 
+
+// int digitcount(long a);
+
+// int main()
+// {
+//     long x=990;
+//     int res;
+
+//     printf("%ld",x);
+//     res=digitcount(x);
+//     printf("\n number %d",res);
+// }
+// int digitcount(long a)
+// {
+//     int count=0;
+//     while (a>0)
+//     {
+//         count++;
+//         a=a/10;
+//     }
+//     return count;
+// }
+
+
+//////////Argument with no return value
+
+// void digitcount(long a);
+
+// int main()
+// {
+//     long x=990;
+//     digitcount(x);
+// }
+// void digitcount(long a)
+// {
+//     int count=0;
+//     while(a>0)
+//     {
+//         count++;
+//         a=a/10;
+//     }
+//     printf("number %d",count );
+// }
+
+///////No argument with retun value
