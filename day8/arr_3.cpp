64e41b7b80ff67a194499e8380f5b20cc9d984e6
Harsh-wd <harshbansal.it26@gmail.com>
2025-06-03 20:08:17 +0530

    more array codes

---
+++ day8/arr_3.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class arr
+{
+public:
+        void func()
+        {
+            int arr[3][3];
+            int r,c;
+            cout<<"enter elements: \n";
+            for(r=0;r<3;r++)
+            {
+                for(c=0;c<3;c++)
+                {
+                    cin>>arr[r][c];
+                }
+            }
+            cout<<"\n----------------"<<endl;
+            for(r=0;r<3;r++)
+            {
+                for(c=0;c<3;c++)
+                {
+                    if((r+c)%2==1 || (r==1 && c==1))
+                    {
+                        cout<<arr[r][c];
+                    }
+                    else
+                    {
+                        cout<<" ";
+                    }
+                    
+                }
+                cout<<"\n";
+            }
+            
+        }
+};
+int main()
+        {
+            arr obj;
+            obj.func();
+        }
