3df92ad596278c726484c48f3c584794b3746041
Harsh-wd <harshbansal.it26@gmail.com>
2025-06-03 20:07:00 +0530

    more array codes

---
+++ day8/arr_2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class arr
+{
+public:
+        void func()
+        {
+            int arr[3][3];
+            int r,c;
+            cout<<"enter elements: \n";
+            for(r=0;r<3;r++)
+            {
+                for(c=0;c<3;c++)
+                {
+                    cin>>arr[r][c];
+                }
+            }
+            cout<<"\n----------Column reverse--------"<<endl;
+            for(r=0;r<3;r++)
+            {
+                for(c=2;c>=0;c--)
+                {
+                    cout<<arr[r][c]<<" ";
+                    
+                }
+                cout<<"\n";
+            }
+            
+        }
+};
+int main()
+        {
+            arr obj;
+            obj.func();
+        }
