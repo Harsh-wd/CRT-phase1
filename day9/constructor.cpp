9f3a958e3500c2293a6062f1236ddc1603e0316b
Harsh-wd <harshbansal.it26@gmail.com>
2025-06-04 20:30:07 +0530

    more constructor added

---
+++ day9/constructor.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class JECRC
+{
+public:
+        JECRC()
+        {
+            cout<<"Welcome"<<endl;
+        }
+        void Btech()
+        {
+            cout<<"btech"<<endl;
+        }
+        void bba()
+        {
+            cout<<"bba"<<endl;
+        }
+        void mba()
+        {
+            cout<<"mba"<<endl;
+        }
+
+    
+};
+int main()
+{
+    JECRC con;
+    JECRC con1;
+    con.bba();
+    con.Btech();
+
+}
