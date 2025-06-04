ad41e2f840d946ce46e4982cc24fe531b7a364a3
Harsh-wd <harshbansal.it26@gmail.com>
2025-06-04 20:30:53 +0530

    destructor added

---
+++ day9/destructor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class cond
+{
+public:
+        cond()
+        {
+            ///constructor
+        }
+        ~cond()
+        {
+            //destructor
+        }
+};
+int main()
+{
+    cond obj;
+
+}
