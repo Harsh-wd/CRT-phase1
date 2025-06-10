d0eddff33870f655226b3dc0f0d9abb4a70a5dcc
Harsh-wd <harshbansal.it26@gmail.com>
2025-06-10 08:19:38 +0530

    even  pattern  added

---
+++ day10/pattern_even.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class pattern
+{
+public:
+        void func()
+        {
+            int arr[3][3];
+            int r,c;
+            cout<<"enter elements\n";
+            for(r=0;r<3;r++)
+            {
+                for(c=0;c<3;c++)
+                {
+                    cin>>arr[r][c];
+                }
+            }
+            cout<<"\narray format-----\n";
+            for(r=0;r<3;r++)
+            {
+                for(c=0;c<3;c++)
+                {
+                    if((arr[r][c]%2==0))
+                    {
+                        cout<<arr[r][c]<<"  ";
+                    }
+                    else
+                    {
+                        cout<<"    ";
+                    }
+                }
+                cout<<"\n";
+            }
+        }
+};
+int  main()
+{
+    pattern obj;
+    obj.func();
+}
